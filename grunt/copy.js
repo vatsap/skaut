18555424933b9d446aed2d7a0a7e76be48156996
Michael Cenker <michael@visionapps.cz>
2016-02-02 15:28:41 +0100

    use local fonts

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,4 +1,16 @@
 'use strict';
 
 module.exports = {
+
+  fonts: {
+    files: [
+      {
+        expand: true,
+        cwd: '<%= paths.src %>/fonts/',
+        src: ['**/*'],
+        dest: '<%= paths.dist %>/fonts/',
+      },
+    ],
+  },
+
 };
