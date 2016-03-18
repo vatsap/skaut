c1c0fdd715050281743d873a2df8af2000ffbb89
Michael Cenker <michael@visionapps.cz>
2016-03-18 14:14:42 +0100

    configure controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,11 +8,38 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
+    public function homepageAction(Request $request)
     {
         return $this->render('index.html.twig');
+    }
+
+    public function troopAction(Request $request)
+    {
+        return $this->render('troop.html.twig');
+    }
+
+    public function eventsAction(Request $request)
+    {
+        return $this->render('events.html.twig');
+    }
+
+    public function galleryAction(Request $request)
+    {
+        return $this->render('gallery.html.twig');
+    }
+
+    public function contactsAction(Request $request)
+    {
+        return $this->render('contacts.html.twig');
+    }
+
+    public function downloadAction(Request $request)
+    {
+        return $this->render('download.html.twig');
+    }
+
+    public function linksAction(Request $request)
+    {
+        return $this->render('links.html.twig');
     }
 }
